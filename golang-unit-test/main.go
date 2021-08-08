3c61035df3f186d0046d39eb34c768912442a248
SaikumarSanisetty <88367900+SaikumarSanisetty@users.noreply.github.com>
2021-08-08 20:00:58 +0530

    Test cases added

---
+++ golang-unit-test/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"./controller"
+	router "./http"
+	"./repository"
+	"./service"
+)
+
+var (
+	postRepository repository.PostRepository = repository.NewFirestoreRepository()
+	postService    service.PostService       = service.NewPostService(postRepository)
+	postController controller.PostController = controller.NewPostController(postService)
+	httpRouter     router.Router             = router.NewChiRouter()
+)
+
+func main() {
+	const port string = ":8000"
+
+	httpRouter.GET("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Up and running...")
+	})
+
+	httpRouter.GET("/posts", postController.GetPosts)
+	httpRouter.POST("/posts", postController.AddPost)
+
+	httpRouter.SERVE(port)
+}
